fd3d351f57d94c7a94302889a6343c94140bd8bb
Noah Taylor <noah9990@yahoo.com>
2017-11-16 09:58:01 -0500

    Further additions to stored database procedures

--- DB/db_procs.sql
+++ DB/db_procs.sql
@@ -12,7 +12,7 @@ CREATE PROCEDURE create_user(
 
       commit;
 
-      SELECT LAST_INSERT_ID() as 'picture_id';
+      SELECT LAST_INSERT_ID() as 'user_id';
 
     ELSE
 
@@ -39,7 +39,7 @@ CREATE PROCEDURE create_picture(
         ANALYSIS,
         CONFIDENCE)
       VALUES (
-        p_user_id,
+        (select user_id from USERS where user_token = p_user_token),
         sysdate(),
         p_analysis,
         p_confidence
@@ -60,24 +60,29 @@ CREATE PROCEDURE create_favorite(
 )
   BEGIN
 
-if valid_token(p_user_token) AND valid_picture(p_picture_id) THEN
+if valid_token(p_user_token)
+    AND valid_picture(p_picture_id)
+    AND not in_favorites(
+      (select user_id from USERS where user_token = p_user_token),
+      p_picture_id
+   )THEN
+
 
     insert into FAVORITES (
       USER_ID,
       PICTURE_ID,
       CREATION_DATE)
     VALUES(
-      p_user_id,
+      (select user_id from USERS where user_token = p_user_token),
       p_picture_id,
       sysdate()
     );
 
       commit;
       SELECT LAST_INSERT_ID() as 'favorite_id';
-   end if;
-
-  select -1 from dual;
-
+    ELSE
+      select -1 from dual;
+end if;
   END;
 
 DROP PROCEDURE IF EXISTS login;
@@ -88,26 +93,28 @@ CREATE PROCEDURE login(
 )
 BEGIN
   declare l_token int;
+
   if valid_password(p_user_name,p_password) THEN
-    select user_token,new_user_token()
+
+    select USER_TOKEN
+    into l_token
     from USERS
     where USER_NAME = p_user_name;
 
-    if(user_token is null) THEN
+    if(l_token is null) THEN
       update USERS
       set USER_TOKEN = new_user_token()
       where USER_NAME = p_user_name;
 
-      select LAST_INSERT_ID() into l_token;
     END IF;
 
-    select l_token from dual;
-
+    select USER_TOKEN from USERS where user_name = p_user_name;
   ELSE
 
     select -1 from dual;
 
   END IF;
+
 END;
 
 DROP FUNCTION IF EXISTS valid_user;
@@ -115,21 +122,10 @@ DROP FUNCTION IF EXISTS valid_user;
 Create FUNCTION valid_user(
   p_user_name varchar(30)) returns boolean
   BEGIN
-    declare valid varchar(1);
-
-    select if(
-        p_user_name in (
-          select user_name from USERS WHERE
+    return exists(
+       select user_name from USERS WHERE
           user_name = p_user_name
-          AND ifnull(end_date,sysdate() + 1) > sysdate()
-        ),'Y','N')
-      into valid;
-
-    if valid = 'Y' THEN
-      return true;
-    END IF;
-
-    return false;
+    );
   END;
 
 drop function if exists new_user_token;
@@ -215,19 +211,30 @@ drop function if exists valid_password;
 create function valid_password(p_user_name varchar(30),p_password varchar(255))
   returns BOOLEAN
   BEGIN
-    declare valid int;
     if (valid_user(p_user_name)) then
-      select count(*) from USERS
-        where user_name = p_user_name
-      AND PASSWORD = p_password
-      into valid;
 
-      if valid = 1 THEN
-        return true;
-      else
-        return false;
-      END IF;
+      return exists(
+        select * from USERS
+        where user_name = p_user_name
+        AND PASSWORD = p_password
+      );
+
     ELSE
       return false;
     end if;
   END;
+
+drop function if EXISTS in_favorites;
+
+create function in_favorites(p_user_id int, p_picture_id  int) returns boolean
+
+  BEGIN
+      return exists(
+        select *
+        from FAVORITES
+        where user_id = p_user_id
+        AND picture_id = p_picture_id
+      );
+  END;
+
+drop function IF EXISTS disable_user;
