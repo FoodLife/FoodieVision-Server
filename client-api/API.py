3d2132c9e0df45b6e639213d5d5d917c7c25d6ac
Noah Taylor <noah9990@yahoo.com>
2017-11-28 16:36:14 -0500

    API adjustments

--- client-api/API.py
+++ client-api/API.py
@@ -32,7 +32,7 @@ def make_user():
 			result = foodie_db.create_user(conn, data["user_name"], data["password"])
 			conn.close()
 			if result > 0:
-				return jsonify(success=1, user_id=1)
+				return jsonify(success=1, result=result)
 			return jsonify(success=0, result="username already in use")
 		return jsonify(success=-1, result="invalid parameters")
 
@@ -75,20 +75,32 @@ def is_food():
 			is_food = 'Y' if ai_result[1] else 'N'
 			result = foodie_db.create_picture(conn,data["user_token"], analysis[0,0], analysis[0,1], is_food)
 			if not result  == 'exception':
-				folder = 'images/' + str(result[0]) + "/"
+				folder = 'images/'
+				thumb_folder = folder + 'thumb/'
 				file_path = folder + str(result[1]) + ".jpg"
+				thumb_path = thumb_folder + str(result[1]) + "_thumb.jpg"
 				if not os.path.isdir(folder):
 					os.makedirs(folder)
+				if not os.path.isdir(thumb_folder):
+					os.makedirs(thumb_folder)
 				image = Image.open(BytesIO(base64.b64decode(data["image"]))).convert('RGB')
 				image.save(file_path)
-				confidence=analysis.tolist()
-                                foodieScore = max(max(confidence))
-				foodieScore=(foodieScore+5)/10
+				image.resize((128, 128), Image.BILINEAR).save(thumb_path)
+
+				confidence = analysis.tolist()
+				foodieScore = max(max(confidence))
+				foodieScore = (foodieScore + 5) / 10
 				if result > 0:
-					return jsonify(success=1, result= is_food, score=foodieScore)
+					return jsonify(success=1, result=is_food, score=foodieScore)
+
 				return jsonify(success=0, result= "invalid user")
 			return jsonify(success=-1,result='db error')
 		return jsonify(success=-1, result= "invalid parameters")
+
+@app.route('/foodies/search',methods=(['POST']))
+def search():
+	if request.method == 'POST':
+		data = request.get_json()
 
 @app.route('/hello', methods=('GET', 'POST'))
 def hello():
@@ -97,6 +109,8 @@ def hello():
 
 		return jsonify(('HELLO WURLD ' + data["json"]), "test")
 
-
+@app.route('/image/<image_name>')
+def get_image():
+	return jsonify(result=-1)
 if __name__ == "__main__":
-	app.run()
+	app.run(host='0.0.0.0')
